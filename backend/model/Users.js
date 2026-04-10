ad5c2b0159d3b6c36ced42f7d079ee56ef3f0c1b
mamme234 <ghazimuhammadilyas@gmail.com>
2026-04-10 15:26:38 +0300

    Update Users.js

--- backend/model/Users.js
+++ backend/model/Users.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
   referralLink: String,
+
   coins: { type: Number, default: 0 },
   usdt: { type: Number, default: 0 },
+
   level: { type: Number, default: 1 },
   referrals: { type: Number, default: 0 },
+
   withdrawUnlocked: { type: Boolean, default: false }
 });
 
